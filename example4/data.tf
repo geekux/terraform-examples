daca7ba50222a7db297fd3af859172d60c82b6dc
Pawel Szatkowski <p.szatkowski.byd@gmail.com>
2019-09-06 07:02:15 +0000

    Updated data.tf

--- example4/data.tf
+++ example4/data.tf
@@ -1,10 +1,10 @@
-data "aws_ami" "amazon-linux-2" {
+data "aws_ami" "amazon-linux-2-x86_64-gp2" {
   most_recent = true
   owners = ["amazon"]
 
   filter {
     name = "name"
-    values = ["amzn2-ami-hvm*"]
+    values = ["amzn2-ami-hvm*-gp2"]
   }
 
   filter {
